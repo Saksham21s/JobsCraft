924066ad15798fe54ba5c367442ee2796d128068
Saksham  Pandey <saksham4200@gmail.com>
2025-03-22 17:34:59 +0530

    Package version corrected

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/App.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Buffer } from 'buffer'; 
+import './styles/App.css';
+import App from './App.jsx';
+
+window.Buffer = Buffer;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
